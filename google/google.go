b106e920dd4ade2e82472a651df58e92856e0067
Ben Adida <ben@clever.com>
2016-06-17 05:50:04 +0000

    transferred functionality to separate google file

--- google/google.go
+++ google/google.go
@@ -4,41 +4,103 @@ package google
 
 import (
 	"bytes"
-	"encoding/json"
+	"encoding/base64"
+	"encoding/gob"
 	"errors"
-	"fmt"
-	"net/http"
-	"reflect"
-
-	"io/ioutil"
 
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v2"
-	"google.golang.org/api/googleapi/transport"
 )
 
 type Doc struct {
-	file *drive.File
+	File *drive.File
 }
 
 type GoogleDocsService interface {
-	CreateFromTemplate(templateDocID string, title string) (*Doc, err)
-	SetDocPermissionTypeRole(doc *Doc, permissionType string, permissionRole string) err
+	CreateFromTemplate(title string) (*Doc, error)
+	SetDocPermissionTypeRole(doc *Doc, permissionType string, permissionRole string) error
 }
 
 type GoogleDocsServer struct {
 	ClientID string
 	ClientSecret string
-	AccessToken string
+	AccessToken *oauth2.Token
+	Service *drive.Service
 	TemplateDocID string
 }
 
 
-func (server *GoogleDocsServer) CreateFromTemplate(templateDocID string, title string) (*Doc, err) {
+func decodeOAuthToken(tokenString string) *oauth2.Token {
+	tokenBytes, _ := base64.StdEncoding.DecodeString(tokenString)
+	tokenBytesBuffer := bytes.NewBuffer(tokenBytes)
+	dec := gob.NewDecoder(tokenBytesBuffer)
+	token := new(oauth2.Token)
+	dec.Decode(token)
+
+	return token
+}
+
+func NewGoogleDocsServer(clientID string, clientSecret string, accessToken string, templateDocID string) (*GoogleDocsServer, error) {
+	token := decodeOAuthToken(accessToken)
+
+	var config = &oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Endpoint:     google.Endpoint,
+		Scopes:       []string{drive.DriveScope},
+	}
+
+	// instantiate the Google Drive client
+	oauthClient := config.Client(context.Background(), token)
+	service, err := drive.New(oauthClient)
+
+	if err != nil {
+		return nil, err
+	}
+	
+	return &GoogleDocsServer{
+		ClientID: clientID,
+		ClientSecret: clientSecret,
+		AccessToken: token,
+		Service: service,
+		TemplateDocID: templateDocID,
+	}, nil
+}
+
+func (server *GoogleDocsServer) CreateFromTemplate(title string) (*Doc, error) {
+	file, err := server.Service.Files.Copy(server.TemplateDocID, &drive.File{
+		Title: title,
+	}).Do()
+
+	if err != nil {
+		return nil, err
+	}
+	
+	return &Doc{
+		File: file,
+	}, nil
 }
 
 
-func (server *GoogleDocsServer) SetDocPermissionTypeRole(doc *Doc, permissionType string, permissionRole string) err {
+func (server *GoogleDocsServer) SetDocPermissionTypeRole(doc *Doc, permissionType string, permissionRole string) error {
+	file := doc.File
+	
+	// make it editable by the entire organization
+	permissions, err := server.Service.Permissions.List(file.Id).Do()
+	if err != nil {
+		return err
+	}
+
+	// look for the right permission and update it to "Writer"
+	for _, perm := range permissions.Items {
+		if perm.Type == permissionType {
+			perm.Role = permissionRole
+			_, err = server.Service.Permissions.Update(file.Id, perm.Id, perm).Do()
+			return err
+		}
+	}
+
+	return errors.New("could not find permission of type " + permissionType)
 }
