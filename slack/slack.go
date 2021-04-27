deaca19432c517f78e42d6f28880efcab2a46647
Jansen Sheng <jansen.sheng@clever.com>
2021-04-27 16:26:32 -0700

    Switching to \W instead of \s

--- slack/slack.go
+++ slack/slack.go
@@ -72,7 +72,7 @@ func (c *Client) Hear(pattern string, fn func(*Message, [][]string)) {
 }
 
 func (c *Client) Respond(pattern string, fn func(*Message, [][]string)) {
-	c.Hear(fmt.Sprintf("<@%s|%s>:?(\\s*|\\\\u00a0)%s", c.Username, c.userId, pattern), fn)
+	c.Hear(fmt.Sprintf("<@%s|%s>:?\\W*%s", c.Username, c.userId, pattern), fn)
 }
 
 func (c *Client) Send(msg, channelId string) {
