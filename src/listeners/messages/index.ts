58031eeba56be44660ffe7fcbd2fc5cd8d9b1afd
Tanmay Sardesai <tanmay.sardesai@clever.com>
2025-07-31 13:19:13 -0700

    feat: Add commsLead message handler for communications lead functionality
    - Create commsLead.ts with message handler implementation
    - Add comprehensive test suite in commsLead.test.ts (93 lines)
    - Update index.ts to export the new commsLead handler
    - Total: 3 files changed, 156 insertions

--- src/listeners/messages/index.ts
+++ src/listeners/messages/index.ts
@@ -2,11 +2,13 @@ import type { App } from "@slack/bolt";
 import { help, helpRegex } from "./help";
 import { fireAFlareRegex, fireFlare } from "./fireFlare";
 import { flareTransitionRegex, flareTransition } from "./flareTransition";
+import { commsLeadRegex, commsLead } from "./commsLead";
 
 const register = (app: App) => {
   app.message(fireAFlareRegex, fireFlare);
   app.message(helpRegex, help);
   app.message(flareTransitionRegex, flareTransition);
+  app.message(commsLeadRegex, commsLead);
 };
 
 export default { register };
